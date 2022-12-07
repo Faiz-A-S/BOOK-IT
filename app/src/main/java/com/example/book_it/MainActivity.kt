381e67589a3eb4136850903187ea0d5665b75144
Delta Revanza <revanzadelta@gmail.com>
2022-12-08 03:08:03 +0700

    bottom nav bar (interaksinya blm, baru fragment sama layout)

--- app/src/main/java/com/example/book_it/MainActivity.kt
+++ app/src/main/java/com/example/book_it/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.book_it
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.lang.Exception
+import android.content.Intent
+import android.os.PersistableBundle
 import android.widget.Button
 //login
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +25,33 @@ class MainActivity : AppCompatActivity() {
             intent = Intent (this, Base::class.java)
             startActivity(intent)
         }
+    }
+
+    fun main_menu() {
+        setContentView(R.layout.main_menu)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnNavigationItemReselectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.message -> {
+                    loadFragment(CartFragment())
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.settings -> {
+                    loadFragment(ProfileFragment())
+                    return@setOnNavigationItemReselectedListener
+                }
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/example/book_it/ProfileFragment.kt
+++ app/src/main/java/com/example/book_it/ProfileFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Chat.newInstance] factory method to
- * create an instance of this fragment
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
  */
-class Chat : Fragment() {
+class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class Chat : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class Chat : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment chat.
+         * @return A new instance of fragment ProfileFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Chat().apply {
+            ProfileFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
