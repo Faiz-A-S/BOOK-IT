97294f66c6769d77dea527a266c726143264a085
Faiz-A-S <faizasucipto@gmail.com>
2022-12-13 14:24:42 +0700

    need to find a way to get downloadURL

--- app/src/main/java/com/example/book_it/UploadBuku.kt
+++ app/src/main/java/com/example/book_it/UploadBuku.kt
@@ -1,33 +1,40 @@
 package com.example.book_it
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.view.LayoutInflater
-import android.widget.ImageView
+import android.provider.OpenableColumns
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.book_it.databinding.TambahBukuBinding
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import java.util.*
 
 class UploadBuku: AppCompatActivity(){
-
+    lateinit var storage: StorageReference
     private lateinit var dbRef: DatabaseReference
     private lateinit var binding: TambahBukuBinding
+    private lateinit var uriImage: Uri
+    private lateinit var uri: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        storage = FirebaseStorage.getInstance().getReference("Cover")
         binding = TambahBukuBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         dbRef = FirebaseDatabase.getInstance().getReference("Book")
-
         binding.imgbuttontambah.setOnClickListener{
             selectImage()
         }
-
         binding.tambahBuku.setOnClickListener{
             tambahBukuData()
         }
@@ -38,15 +45,21 @@ class UploadBuku: AppCompatActivity(){
         pickImage.launch(intent)
     }
 
-    val pickImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+    private val pickImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
         result ->
         if(result.resultCode == RESULT_OK){
-            val uriImage = result.data?.data
+            uriImage = result.data?.data!!
             binding.imgbuttontambah.setImageURI(uriImage)
+            val sd = getFileName(applicationContext, uriImage)
+            val storageRef = storage.child("coverbuku/$sd")
+            var uploadTask = storageRef.putFile(uriImage)
+
+            uri = uploadTask.snapshot.storage.downloadUrl.toString()
         }
     }
 
     private fun tambahBukuData() {
+        //val gambar
         val judul = binding.judulBuku.text.toString()
         val harga = binding.hargaBuku.text.toString().toInt()
         val penulis = binding.penulisBuku.text.toString()
@@ -66,13 +79,27 @@ class UploadBuku: AppCompatActivity(){
         }
 
         val bukuID = dbRef.push().key!!
-
-        val uploadBukuBaru = UploadBukuModel(judul,harga,penulis,desk)
+        val uploadBukuBaru = UploadBukuModel(uri, judul, harga, penulis, desk)
+        //val uploadBukuBaru = UploadBukuModel(storage.downloadUrl.toString(),judul,harga,penulis,desk)
         dbRef.child(bukuID).setValue((uploadBukuBaru))
             .addOnCompleteListener{
                 Toast.makeText(this,"Nice",Toast.LENGTH_SHORT).show()
             }.addOnFailureListener{err ->
         Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show()
     }
+    }
+    @SuppressLint("Range")
+    private fun getFileName(context: Context, uri: Uri): String? {
+        if (uri.scheme == "content") {
+            val cursor = context.contentResolver.query(uri, null, null, null, null)
+            cursor.use {
+                if (cursor != null) {
+                    if(cursor.moveToFirst()) {
+                        return cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                    }
+                }
+            }
+        }
+        return uri.path?.lastIndexOf('/')?.let { uri.path?.substring(it) }
     }
 }
--- app/src/main/java/com/example/book_it/UploadBukuModel.kt
+++ app/src/main/java/com/example/book_it/UploadBukuModel.kt
@@ -1,6 +1,9 @@
 package com.example.book_it
 
+import android.net.Uri
+
 data class UploadBukuModel(
+    var Gambar:String? = null,
     var Judul:String? = null,
     var Harga:Int? = null,
     var Penulis:String? = null,
