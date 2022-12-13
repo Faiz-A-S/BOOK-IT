00683a5ca09ecde0c0cecf3a70ef433647f419f9
Delta Revanza <revanzadelta@gmail.com>
2022-12-13 17:35:36 +0700

    nyoba registernya, masi blm bisa masuk ke db (kyknya UID nya ga ke generate) sama touch-up dikit di bakian tombol register sama back to login

--- app/src/main/java/com/example/book_it/Register.kt
+++ app/src/main/java/com/example/book_it/Register.kt
@@ -6,10 +6,28 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.book_it.databinding.RegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseReference
 
 class Register: AppCompatActivity() {
     private lateinit var binding : RegisterBinding
     private lateinit var firebaseAuth: FirebaseAuth
+
+    fun isEmail(input: String): Boolean {
+        // Set up the regular expression to match email addresses
+        val regex = Regex("^[A-Za-z0-9+_.-]+@(.+)$")
+
+        // Use the `matches` method to check if the input string matches the regular expression
+        return regex.matches(input)
+    }
+
+    fun registerUser(email:String,password:String){
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("users")
+
+        val user = User(email, password) // create a new user object
+        ref.push().setValue(user) // save the user to the "users" node in the database
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,22 +48,27 @@ class Register: AppCompatActivity() {
             val confirmpass = binding.registerPassword2.text.toString()
 
             if(email.isNotEmpty() && password.isNotEmpty() && confirmpass.isNotEmpty()){
-                if(password == confirmpass){
-                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
-                        if(it.isSuccessful){
-                            val intent = Intent(this, Login::class.java)
-                            startActivity(intent)
+                if(isEmail(email)==true){
+                    if(password == confirmpass) {
+                        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                                if (it.isSuccessful) {
+                                    val intent = Intent(this, Login::class.java)
+                                    registerUser(email,password)
+                                    startActivity(intent)
 
-                        }else{
-                            Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
-                    }}
-
+                                } else {
+                                    Toast.makeText(
+                                        this,"Registrasi berhasil", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                    }else{
+                        Toast.makeText(this,"Password tidak sama", Toast.LENGTH_SHORT).show()
+                    }
                 }else{
-                    Toast.makeText(this,"Password tidak sama", Toast.LENGTH_SHORT).show()
-                }
+                    Toast.makeText(this,"Email tidak sesuai", Toast.LENGTH_SHORT).show()
+            }
             }else {
                 Toast.makeText(this, "Isi semua field", Toast.LENGTH_SHORT).show()
-            }
         }
     }
-}
+}}
---
+++ app/src/main/java/com/example/book_it/User.kt
@@ -0,0 +1,3 @@
+package com.example.book_it
+
+data class User(val email: String, val password: String)
